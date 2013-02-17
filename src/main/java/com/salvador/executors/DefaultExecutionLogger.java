481662e0678067bcda613479c84519703a4e9a7f
mearlam <nufc2002>
2013-02-17 20:06:28 +0000

    moved log to a history folder

--- src/main/java/com/salvador/executors/DefaultExecutionLogger.java
+++ src/main/java/com/salvador/executors/DefaultExecutionLogger.java
@@ -2,12 +2,17 @@ package com.salvador.executors;
 
 import com.salvador.scenarios.Scenario;
 import com.salvador.scenarios.ScenarioStep;
+import org.apache.commons.io.FileUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.io.PrintWriter;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 
 /**
  * Created with IntelliJ IDEA.
@@ -18,15 +23,25 @@ import java.io.PrintWriter;
 public class DefaultExecutionLogger implements ExecutionLogger {
 
     static final Logger log = LoggerFactory.getLogger(DefaultExecutionLogger.class);
+    SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd HH-mm-ss");
 
     PrintWriter out;
 
     public DefaultExecutionLogger(String home) {
         try {
-            final String outputFile = home + File.separator + "test-output.log";
+            Calendar now = Calendar.getInstance();
+
+            final String outputFolder = home + File.separator + "history" + File.separator + format.format(now.getTime());
+            File folder = new File(outputFolder);
+
+            FileUtils.forceMkdir(folder);
+            final String outputFile = outputFolder + File.separator + "tests.txt" ;
             out = new PrintWriter(outputFile);
             log.debug("Writing output to {}", outputFile);
+
         } catch (FileNotFoundException e) {
+            log.error("Could not create log file", e);
+        } catch (IOException e) {
             log.error("Could not create log file", e);
         }
     }
